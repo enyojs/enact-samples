051fab9c20014c2e0edb2f77d8baa666c1efec5d
Stanca <stanca.pop@lgepartner.com>
2024-07-12 13:36:47 +0300

    created extractComponentProps function to extract component props from given design, adjusted components to different resolutions, adjusted functions for imports and creation of components to deal with specific components

--- sandstone/plugin-enact-figma/code.ts
+++ sandstone/plugin-enact-figma/code.ts
@@ -1,65 +1,85 @@
 // Show the UI to the user
-figma.showUI(__html__, {width: 600, height: 400});
+figma.showUI(__html__, { width: 600, height: 400 });
 
 // Listen for messages from the UI
 figma.ui.onmessage = (msg) => {
   if (msg.type === 'create') {
     const components = (figma.currentPage.children[0] as FrameNode).children.map(children => {
-      const props = children.name === 'Button' ? {childrensProps: (children as ComponentNode | InstanceNode).children} : {}
-      return { name: children.name, props: props, width: children.width, height: children.height, x: children.x, y: children.y }
+      const componentProps = extractComponentProps(children);
+      const childrenProps = componentProps.children;
+      const componentName = componentProps.parent.name;
+      return { componentName, componentProps, childrenProps, x: children.x, y: children.y }
     });
-
     writeFile(mainContent(components))
   }
 };
 
-const convertToRGB = (r, g, b) => {
-  const red = Math.round(r * 255);
-  const green = Math.round(g * 255);
-  const blue = Math.round(b * 255);
+const extractComponentProps = (component) => {
+  if (typeof component.children !== 'undefined') {
+    return extractComponentProps(component.children[0]);
+  }
 
-  return {red, green, blue}
+  return component.parent;
 }
 
-const createImports = (components: { name: string, x: number, y: number }[]) => {
-  const imports = [...new Set(components.map(component => component.name))].map(name => {
-    if (name === 'Screen') return;
-    return `import {${name}} from '@enact/sandstone/${name}';\n`
+const convertToRGB = (color: { r: number, g: number, b: number }) => {
+  const red = Math.round(color.r * 255);
+  const green = Math.round(color.g * 255);
+  const blue = Math.round(color.b * 255);
+
+  return {red, green, blue};
+}
+
+const createImports = (components: { componentName: string }[]) => {
+  const imports = [...new Set(components.map(component => component.componentName))].map(name => {
+    if (name === 'Header') {
+      return `import { ${name} } from '@enact/sandstone/Panels';\n`
+    } else {
+      return `import { ${name} } from '@enact/sandstone/${name}';\n`
+    }
   });
   return imports.toString().replace(/,/g, '');
 }
 
 const createComponents = (components) => {
-  const allComponents = components.map((component) => {
-    if (component.name === 'Screen') return;
-    const componentColors = component.props?.childrensProps[0].fills[0].color;
-    const convertedComponentColors = convertToRGB(componentColors.r, componentColors.g, componentColors.b);
-    const textNode = component.props?.childrensProps[1].characters;
-    const textColors = component.props?.childrensProps[1].fills[0].color;
-    const convertedTextColors = convertToRGB(textColors.r, textColors.g, textColors.b);
-    return `<${component.name} style={{backgroundColor: 'rgb(${convertedComponentColors.red}, ${convertedComponentColors.green}, ${convertedComponentColors.blue})', color: 'rgb(${convertedTextColors.red}, ${convertedTextColors.green}, ${convertedTextColors.blue})', position: 'absolute', left: '${component.x}px', top: '${component.y}px', width: ${component.width}, height: ${component.height}}}>${textNode}</${component.name}>\n`
+  const allComponents = components.map((component, index) => {
+    const componentColors = component.componentProps.fills.length > 0 ? convertToRGB(component.componentProps.fills[0].color) : undefined;
+    const childrenColors = component.childrenProps.map(childrenProps => childrenProps.fills.length > 0 ? convertToRGB(childrenProps.fills[0].color) : undefined);
+    const childrenText = component.childrenProps.map(childrenProps => childrenProps.characters);
+    let componentNode;
+    switch (component.componentName) {
+      case 'Button': componentNode = `<${component.componentName} style={{height: ri.scaleToRem(${component.componentProps.height}), width: ri.scaleToRem(${component.componentProps.width}), backgroundColor: 'rgb(${componentColors.red}, ${componentColors.green}, ${componentColors.blue})', color: 'rgb(${childrenColors[0].red}, ${childrenColors[0].green}, ${childrenColors[0].blue})', position: 'absolute', left: ri.scaleToRem(${component.x}), top: ri.scaleToRem(${component.y})}}>${childrenText[0]}</${component.componentName}>`
+            break;
+      case 'Header': componentNode = `<${component.componentName} style={{height: ri.scaleToRem(${component.componentProps.height}), width: ri.scaleToRem(${component.componentProps.width}), position: 'absolute', left: ri.scaleToRem(${component.x}), top: ri.scaleToRem(${component.y})}} subtitle='${childrenText[1]}' title='${childrenText[0]}' />`
+        break;
+      case 'Input': componentNode = `<${component.componentName} style={{height: ri.scaleToRem(${component.componentProps.height}), width: ri.scaleToRem(${component.componentProps.width}), backgroundColor: 'rgb(${componentColors.red}, ${componentColors.green}, ${componentColors.blue})', color: 'rgb(${childrenColors[0].red}, ${childrenColors[0].green}, ${childrenColors[0].blue})', position: 'absolute', left: ri.scaleToRem(${component.x}), top: ri.scaleToRem(${component.y})}} placeholder='${childrenText[0]}' />`
+        break;
+    }
+    if (index < components.length - 1) componentNode += '\n';
+    return componentNode;
   });
-  return allComponents.toString().replace(/,</g, '<');
+  return allComponents.toString().replace(/,</g, '\t\t\t<');
 }
 
 const mainContent = (components) => {
   return `${createImports(components)}
 import kind from '@enact/core/kind';
 import {Panel} from '@enact/sandstone/Panels';
+import ri from '@enact/ui/resolution';
 
 const MainPanel = kind({
-name: 'MainPanel',
+    name: 'MainPanel',
 
-render: (props) => (
-    <Panel {...props}>
-        ${createComponents(components)}
-    </Panel>
-)
+    render: (props) => (
+        <Panel {...props}>
+            ${createComponents(components)}
+        </Panel>
+    )
 });
 
 export default MainPanel;`
 }
 
 const writeFile = (content) => {
-  figma.ui.postMessage({type: 'show-code', data: content })
+  figma.ui.postMessage({type: 'show-code', data: content})
 }
--- sandstone/plugin-enact-figma/enact-figma-app/src/views/MainPanel.js
+++ sandstone/plugin-enact-figma/enact-figma-app/src/views/MainPanel.js
@@ -1,16 +1,21 @@
-import {Button} from '@enact/sandstone/Button';
+import { Button } from '@enact/sandstone/Button';
+import { Input } from '@enact/sandstone/Input';
+import { Header } from '@enact/sandstone/Panels';
 
 import kind from '@enact/core/kind';
-import {Panel} from '@enact/sandstone/Panels';
+import { Panel } from '@enact/sandstone/Panels';
+import ri from '@enact/ui/resolution';
 
 const MainPanel = kind({
 name: 'MainPanel',
 
 render: (props) => (
 <Panel {...props}>
-<Button style={{backgroundColor: 'rgb(217, 217, 217)', color: 'rgb(19, 126, 107)', position: 'absolute', left: '306px', top: '242px', width: 363, height: 121}}>Click it</Button>
-<Button style={{backgroundColor: 'rgb(217, 217, 217)', color: 'rgb(232, 74, 188)', position: 'absolute', left: '960px', top: '242px', width: 363, height: 121}}>Click it</Button>
-
+<Button style={{height: ri.scaleToRem(144), width: ri.scaleToRem(540), backgroundColor: 'rgb(125, 132, 140)', color: 'rgb(230, 230, 230)', position: 'absolute', left: ri.scaleToRem(190), top: ri.scaleToRem(737)}}>Label</Button>
+<Button style={{height: ri.scaleToRem(144), width: ri.scaleToRem(540), backgroundColor: 'rgb(125, 132, 140)', color: 'rgb(230, 230, 230)', position: 'absolute', left: ri.scaleToRem(190), top: ri.scaleToRem(1008)}}>Label</Button>
+<Input style={{height: ri.scaleToRem(168), width: ri.scaleToRem(1800), backgroundColor: 'rgb(125, 132, 140)', color: 'rgb(230, 230, 230)', position: 'absolute', left: ri.scaleToRem(990), top: ri.scaleToRem(996)}} placeholder='Please enter a text' />
+<Header style={{height: ri.scaleToRem(600), width: ri.scaleToRem(3840), position: 'absolute', left: ri.scaleToRem(0), top: ri.scaleToRem(0)}} subtitle='Subtitle' title='Header title' />
+<Button style={{height: ri.scaleToRem(144), width: ri.scaleToRem(540), backgroundColor: 'rgb(125, 132, 140)', color: 'rgb(230, 230, 230)', position: 'absolute', left: ri.scaleToRem(190), top: ri.scaleToRem(1279)}}>Label</Button>
 </Panel>
 )
 });
