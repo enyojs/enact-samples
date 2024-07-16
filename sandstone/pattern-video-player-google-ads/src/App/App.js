de746fa9f693413d4837de96b877033379bbc0a7
Jiye Kim <jiye.kim@lge.com>
2024-07-16 18:35:02 +0900

    fix lint error

--- sandstone/pattern-video-player-google-ads/src/App/App.js
+++ sandstone/pattern-video-player-google-ads/src/App/App.js
@@ -18,92 +18,91 @@ const AppBase = ({className, ...rest}) => {
 	const videoRef = useRef(null);
 
 	const initializeIMA = useCallback(() => {
-        if (window.google) {
+		if (window.google) {
 			const videoElement = videoRef.current.getVideoNode().media;
-			const onAdError = (e) => {
-                console.log(e.getError());
-                if(adsManager) {
-                    adsManager.destroy();
-                }
-            };
+			const onAdError = () => {
+				if (adsManager) {
+					adsManager.destroy();
+				}
+			};
 			const onAdLoaded = (e) => {
-                const ad = e.getAd();
-                if (!ad.isLinear()) {
-                    videoElement.play();
-                }
-            }
+				const ad = e.getAd();
+				if (!ad.isLinear()) {
+					videoElement.play();
+				}
+			};
 			const onContentPauseRequested = () => {
-                videoElement.style.opacity = 0;
+				videoElement.style.opacity = 0;
 				videoElement.pause();
 				setPlayButtonDisabled(true);
 			};
-            const onContentResumeRequested = () => {
+			const onContentResumeRequested = () => {
 				videoElement.style.opacity = 1;
 				videoElement.play();
 				setPlayButtonDisabled(false);
 			};
 			const onAdsManagerLoaded = (e) => {
-                const _adsManager = e.getAdsManager(videoElement);
-                _adsManager.addEventListener(window.google.ima.AdErrorEvent.Type.AD_ERROR,
-                    onAdError);
-                _adsManager.addEventListener(window.google.ima.AdEvent.Type.CONTENT_PAUSE_REQUESTED, onContentPauseRequested);
-                _adsManager.addEventListener(window.google.ima.AdEvent.Type.CONTENT_RESUME_REQUESTED, onContentResumeRequested);
-                _adsManager.addEventListener(window.google.ima.AdEvent.Type.LOADED, onAdLoaded);
-                setAdsManager(_adsManager);
+				const _adsManager = e.getAdsManager(videoElement);
+				_adsManager.addEventListener(window.google.ima.AdErrorEvent.Type.AD_ERROR,
+					onAdError);
+				_adsManager.addEventListener(window.google.ima.AdEvent.Type.CONTENT_PAUSE_REQUESTED, onContentPauseRequested);
+				_adsManager.addEventListener(window.google.ima.AdEvent.Type.CONTENT_RESUME_REQUESTED, onContentResumeRequested);
+				_adsManager.addEventListener(window.google.ima.AdEvent.Type.LOADED, onAdLoaded);
+				setAdsManager(_adsManager);
 				setPlayButtonDisabled(false);
-            };
+			};
 
-            const _adDisplayContainer = new window.google.ima.AdDisplayContainer(adsRef.current, videoElement);
-            const adsLoader = new window.google.ima.AdsLoader(_adDisplayContainer);
-            const adsRequest = new window.google.ima.AdsRequest();
+			const _adDisplayContainer = new window.google.ima.AdDisplayContainer(adsRef.current, videoElement);
+			const adsLoader = new window.google.ima.AdsLoader(_adDisplayContainer);
+			const adsRequest = new window.google.ima.AdsRequest();
 
 			adsLoader.addEventListener(window.google.ima.AdsManagerLoadedEvent.Type.ADS_MANAGER_LOADED, onAdsManagerLoaded, false);
-            adsLoader.addEventListener(window.google.ima.AdErrorEvent.Type.AD_ERROR, onAdError, false);
-            videoElement.addEventListener('ended', () => {
-                adsLoader.contentComplete();
-            });
+			adsLoader.addEventListener(window.google.ima.AdErrorEvent.Type.AD_ERROR, onAdError, false);
+			videoElement.addEventListener('ended', () => {
+				adsLoader.contentComplete();
+			});
 
-            adsRequest.adTagUrl = 'https://pubads.g.doubleclick.net/gampad/ads?' +
-                'iu=/21775744923/external/single_ad_samples&sz=640x480&' +
-                'cust_params=sample_ct%3Dlinear&ciu_szs=300x250%2C728x90&' +
-                'gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=';
+			adsRequest.adTagUrl = 'https://pubads.g.doubleclick.net/gampad/ads?' +
+				'iu=/21775744923/external/single_ad_samples&sz=640x480&' +
+				'cust_params=sample_ct%3Dlinear&ciu_szs=300x250%2C728x90&' +
+				'gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=';
 
-            adsRequest.linearAdSlotWidth = videoElement.clientWidth;
-            adsRequest.linearAdSlotHeight = videoElement.clientHeight;
-            adsRequest.nonLinearAdSlotWidth = videoElement.clientWidth / 3;
-            adsRequest.nonLinearAdSlotHeight = videoElement.clientHeight / 3;
+			adsRequest.linearAdSlotWidth = videoElement.clientWidth;
+			adsRequest.linearAdSlotHeight = videoElement.clientHeight;
+			adsRequest.nonLinearAdSlotWidth = videoElement.clientWidth / 3;
+			adsRequest.nonLinearAdSlotHeight = videoElement.clientHeight / 3;
 
-            adsLoader.requestAds(adsRequest);
+			adsLoader.requestAds(adsRequest);
 
-            setAdDisplayContainer(_adDisplayContainer);
-        }
-    }, [adsRef, adsManager]);
+			setAdDisplayContainer(_adDisplayContainer);
+		}
+	}, [adsRef, adsManager]);
 
-    const onPlay = useCallback(() => {
-        if (adsLoaded) {
-            return;
-        }
+	const onPlay = useCallback(() => {
+		if (adsLoaded) {
+			return;
+		}
 		const videoElement = videoRef.current.getVideoNode().media;
-        adDisplayContainer.initialize();
-        const width = videoElement.clientWidth;
-        const height = videoElement.clientHeight;
-        try {
-            adsManager.init(width, height, window.google.ima.ViewMode.NORMAL);
-            adsManager.start();
-        } catch (adError) {
-            videoElement.play();
-        }
+		adDisplayContainer.initialize();
+		const width = videoElement.clientWidth;
+		const height = videoElement.clientHeight;
+		try {
+			adsManager.init(width, height, window.google.ima.ViewMode.NORMAL);
+			adsManager.start();
+		} catch (adError) {
+			videoElement.play();
+		}
 		setAdsLoaded(true);
-    }, [adDisplayContainer, adsLoaded, adsManager, videoRef]);
+	}, [adDisplayContainer, adsLoaded, adsManager, videoRef]);
 
 	useEffect(() => {
-        const script = document.createElement('script');
-        script.src = "https://imasdk.googleapis.com/js/sdkloader/ima3.js";
-        document.body.appendChild(script);
-        return () => {
-            document.body.removeChild(script);
-        }
-    }, []);
+		const script = document.createElement('script');
+		script.src = "https://imasdk.googleapis.com/js/sdkloader/ima3.js";
+		document.body.appendChild(script);
+		return () => {
+			document.body.removeChild(script);
+		};
+	}, []);
 
 	useEffect(() => {
 		window.onload = () => {
@@ -111,12 +110,12 @@ const AppBase = ({className, ...rest}) => {
 		};
 	}, [initializeIMA]);
 
-    useEffect(() => {
-        const videoElement = videoRef.current.getVideoNode().media;
-        window.onresize = () => {
-            adsManager?.resize(videoElement.clientWidth, videoElement.clientHeight, window.google.ima.ViewMode.NORMAL);
-        };
-    }, [adsManager, videoRef]);
+	useEffect(() => {
+		const videoElement = videoRef.current.getVideoNode().media;
+		window.onresize = () => {
+			adsManager?.resize(videoElement.clientWidth, videoElement.clientHeight, window.google.ima.ViewMode.NORMAL);
+		};
+	}, [adsManager, videoRef]);
 
 	const {source, ...restVideo} = getVideo(0);
 
@@ -124,7 +123,7 @@ const AppBase = ({className, ...rest}) => {
 		<div {...rest} className={className + ' ' + css.app} >
 			<div ref={adsRef} className={css.adContainer} />
 			<VideoPlayer {...restVideo} className={css.player + ' enact-fit'} ref={videoRef} noAutoPlay onPlay={onPlay} >
-                <source src={source} type="video/mp4" />
+				<source src={source} type="video/mp4" />
 				<MediaControls playPauseButtonDisabled={playButtonDisabled} />
 			</VideoPlayer>
 		</div>
