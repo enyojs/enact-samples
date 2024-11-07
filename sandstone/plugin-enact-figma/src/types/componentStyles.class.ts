31bda0da86165e9cd7a714320b092332877cf40e
ion-andrusciac-lgp <122601449+ion-andrusciac-lgp@users.noreply.github.com>
2024-11-07 16:37:44 +0200

    WRR-7940: Enhanced Figma and Sandstone integration - prop change and resolutions (#244)
    * Changed plugin layout, added screen resolutions and refactored extract functions
    
    * Added `borderRadius, fontSize, opacity, padding...` to styles and method to convert generated styles to string
    
    * Fixed lint warnings
    
    * review fixes
    
    ---------
    
    Co-authored-by: Stanca <stanca.pop@lgepartner.com>

---
+++ sandstone/plugin-enact-figma/src/types/componentStyles.class.ts
@@ -0,0 +1,14 @@
+class CustomComponentStyles {
+	backgroundColor?: string;
+	borderRadius?: string;
+	color?: string;
+	fontSize?: string;
+	height?: string;
+	left?: string;
+	opacity?: string;
+	padding?: string;
+	top?: string;
+	width?: string;
+}
+
+export default CustomComponentStyles;
--- sandstone/plugin-enact-figma/src/utils/createComponentNode.ts
+++ sandstone/plugin-enact-figma/src/utils/createComponentNode.ts
@@ -1,5 +1,6 @@
-import CustomComponent from '../types/component.class';
-import EnactComponentNode from '../types/componentNode.class';
+import {CustomComponent, CustomComponentProperties, CustomComponentStyles, EnactComponentNode} from "../types";
+
+const componentsNames = ['CheckboxItem', 'FormCheckboxItem']; // Components names for conditional properties extraction
 
 // Convert Figma RGB color to CSS RGB color
 const convertToRGB = (color: { r: number, g: number, b: number }) => {
@@ -7,59 +8,79 @@ const convertToRGB = (color: { r: number, g: number, b: number }) => {
 	const green = Math.round(color.g * 255);
 	const blue = Math.round(color.b * 255);
 
-	return {red, green, blue};
+	return `rgb(${red}, ${green}, ${blue})`;
 };
 
 // Extract component props from Figma design
-const extractComponentProps = (component: CustomComponent) => {
-	const componentsNames = ['CheckboxItem', 'FormCheckboxItem']; // Components names for conditional properties extraction
-
-	// eslint-disable-next-line no-undefined
-	const componentColors = component.componentProps.fills[0] !== undefined ? convertToRGB(component.componentProps.fills[0].color) : undefined;
-	const childrenColors = component.childrenProps.map(childrenProps => {
-		if (componentsNames.includes(component.componentName) && !!childrenProps.children[0].children) {
-			return convertToRGB(childrenProps.children[0].children[0].fills[0].color);
-		}
-
-		if (childrenProps.fills.length > 0) {
-			return convertToRGB(childrenProps.fills[0].color);
-		}
-
-		// eslint-disable-next-line no-undefined
-		return undefined;
-	});
-	const childrenComponents = component.childrenProps.map((childrenProps, index) => {
+const extractChildComponents = (component: CustomComponent) => {
+	return component.children.map((children, index: number) => {
 		if (componentsNames.includes(component.componentName) && index === 1) {
-			return childrenProps.children[0].children[0].characters ?? childrenProps;
+			return children.children[0].children[0].characters ?? children;
 		}
 
-		return childrenProps.characters ?? childrenProps;
+		return children.characters ?? children;
 	});
+};
 
-	return {componentColors, childrenColors, childrenComponents};
+const getComponentColor = (component: CustomComponent): string => {
+	return component.children.map((children) => {
+		if (componentsNames.includes(component.componentName) && !!children.children[0].children) {
+			return convertToRGB(children.children[0].children[0].fills[0].color);
+		}
+
+		if (children.fills.length > 0) {
+			return convertToRGB(children.fills[0].color);
+		}
+
+		return '';
+	})[0];
+};
+
+const getComponentPadding = (componentProps: InstanceNode): string => {
+	const {paddingBottom = 0, paddingLeft = 0, paddingRight = 0, paddingTop = 0} = componentProps;
+
+	return Object.entries({paddingTop, paddingRight, paddingBottom, paddingLeft})
+		.filter(([, value]) => value)
+		.map(([key, value]) => `${key}: ri.scaleToRem(${value})`)
+		.join(", ");
+};
+
+const extractComponentStyles = (component: CustomComponent): CustomComponentStyles => {
+	const componentFontSize = (component.componentProps.children.find(value => (value as TextNode).fontSize) as TextNode).fontSize;
+
+	const backgroundColor = component.componentProps.fills[0] && convertToRGB(component.componentProps.fills[0].color);
+	const borderRadius = String(component.componentProps.cornerRadius);
+	const color = getComponentColor(component);
+	const fontSize = componentFontSize && `ri.scaleToRem(${Number(componentFontSize)})`;
+	const height = component.componentProps.height && `ri.scaleToRem(${component.componentProps.height})`;
+	const left = component.x && `ri.scaleToRem(${component.x})`;
+	const opacity = String(component.componentProps.opacity);
+	const padding = getComponentPadding(component.componentProps);
+	const top = component.y && `ri.scaleToRem(${component.y})`;
+	const width = component.componentProps.width && `ri.scaleToRem(${component.componentProps.width})`;
+
+	return {backgroundColor, borderRadius, color, fontSize, height, left, opacity, padding, top, width};
+};
+
+const extractComponentProps = (component: CustomComponent, childrenComponents): CustomComponentProperties => {
+	const disabled = component.componentProps.componentProperties.State.value === 'deactivated';
+	const placeholder = childrenComponents[0] ?? '';
+	const selected = component.componentProps.componentProperties.Type?.value === 'selected';
+	const size = component.componentProps.componentProperties.Size?.value;
+	const subtitle = childrenComponents[1] ?? '';
+	const title = childrenComponents[0] ?? '';
+
+	return {disabled, placeholder, selected, size, subtitle, title};
 };
 
 // Create Enact component from Figma component
 const createComponentNode = (component: CustomComponent) => {
-	const {componentColors, childrenColors, childrenComponents} = extractComponentProps(component);
-	const {componentProps} = component;
+	const childComponents = extractChildComponents(component);
+	const componentStyles = extractComponentStyles(component);
+	const componentProps = extractComponentProps(component, childComponents);
 
-	const {componentName, x, y} = component;
-	const {height, width} = component.componentProps;
-
-	const componentStyles = {
-		backgroundColor: componentColors,
-		color: childrenColors,
-		top: y,
-		left: x,
-		height: height,
-		width: width
-	};
-
-	const componentNode = new EnactComponentNode(childrenComponents, componentName);
-	componentNode.createComponent()
-		.addComponentStyle(componentStyles)
-		.addComponentProps(componentProps);
+	const componentNode = new EnactComponentNode(component.componentName);
+	componentNode.createComponent(childComponents).addComponentStyle(componentStyles).addComponentProps(componentProps);
 
 	const generatedNode = componentNode.generatedComponentNode;
 	return generatedNode ? generatedNode.replace(/(\r\n|\n|\r|\t)/gm, "") : '';
